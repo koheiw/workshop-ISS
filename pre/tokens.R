3f7b4d03e3c5dd42c3bf4eb4d56f3465161da73a
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-09-11 21:52:59 +0900

    Show important arguments

--- pre/tokens.R
+++ pre/tokens.R
@@ -7,7 +7,7 @@ toks <- tokens(corp, remove_punct = TRUE, remove_numbers = TRUE,
                padding = TRUE, concatenator = "") %>% 
   tokens_remove(stopwords("ja", "marimo"), padding = TRUE, min_nchar = 2)
 
-col <- textstat_collocations(toks)
+col <- textstat_collocations(toks, min_count = 2)
 toks <- tokens_compound(toks, col[col$z > 3,], join = FALSE, keep_unigrams = TRUE)
 
 saveRDS(toks, "data/tokens_speech.RDS")
