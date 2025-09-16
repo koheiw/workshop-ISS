43cd2f10c4b926521f219bfd187b6d0179cdd595
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-09-16 16:44:11 +0900

    Tidy up

--- pre/tokens.R
+++ pre/tokens.R
@@ -5,7 +5,7 @@ corp <- readRDS("data/corpus_speech.RDS")
 
 toks <- tokens(corp, remove_punct = TRUE, remove_numbers = TRUE,
                padding = TRUE, concatenator = "") %>% 
-  tokens_remove(stopwords("ja", "marimo"), padding = TRUE, min_nchar = 2)
+  tokens_remove(stopwords("ja", "marimo"), min_nchar = 2, padding = TRUE)
 
 col <- textstat_collocations(toks, min_count = 2)
 toks <- tokens_compound(toks, col[col$z > 3,], join = FALSE, keep_unigrams = TRUE)
