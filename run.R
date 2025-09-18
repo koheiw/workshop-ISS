38a90ea441b88e6df1d53cfe6fe5d66387f8773b
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-09-18 09:16:48 +0900

    Add comments

--- run.R
+++ run.R
@@ -1,11 +1,25 @@
+# download files
 #source("pre/download.R")
+
+# create a corpus object
 source("pre/corpus.R")
+
+# tokenize texts
 source("pre/tokens.R")
+
+# dictionary analysis
 source("dictionary.R")
+
+# sentiment analysis
 source("lss.R")
+
+# topic analysis (unseeded)
 source("lda.R")
+
+# topic analysis (seeded)
 source("slda.R")
 
+# create RMD reports
 # rmarkdown::render("report/dictionary.Rmd", knit_root_dir = "../")
 # rmarkdown::render("report/lss.Rmd", knit_root_dir = "../")
 # rmarkdown::render("report/slda.Rmd", knit_root_dir = "../")
